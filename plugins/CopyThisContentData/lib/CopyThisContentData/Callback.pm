909d289bb6b5ba51fb6c06c538027379f3f2d96a
Masahiro Iuchi <miuchi@sixapart.com>
2019-07-03 22:05:14 +0900

    Suppress warnings

--- plugins/CopyThisContentData/lib/CopyThisContentData/Callback.pm
+++ plugins/CopyThisContentData/lib/CopyThisContentData/Callback.pm
@@ -5,10 +5,10 @@ use warnings;
 use JSON ();
 
 sub pre_run {
-    my ($cb, $app) = @_;
+    my ( $cb, $app ) = @_;
     return
-        unless $app->param('__mode') eq 'view'
-        && $app->param('_type') eq 'content_data';
+        unless ( $app->param('__mode') || '' ) eq 'view'
+        && ( $app->param('_type') || '' ) eq 'content_data';
     my $orig_id = $app->param('origin') or return;
     my $origin = $app->model('content_data')->load($orig_id) or return;
     $app->param( 'reedit', 1 );
